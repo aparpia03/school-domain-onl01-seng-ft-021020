97f153cee1bc47f9a3aa3e956d9e7c395a702f1b
Abbas Parpia <aparpia03@gmail.com>
2020-02-20 01:44:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
-# code here!
+#
+class School 
+  
+  ROSTER = {}
+  
+  def initialize(name)
+    @name = name 
+  end 
+  
+  def name
+    @name 
+  end 
+  
+  def add_student(@name, grade)
+    
+  end 
+  
+  
+end   
